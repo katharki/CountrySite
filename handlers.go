067defcd3ef2add8d334fa689f86cff15764278b
Katharina Kivle <katharki@stud.ntnu.no>
2025-03-05 10:02:11 +0100

    jobber med handlers

--- handlers.go
+++ handlers.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func countryInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("You have requested URL: %s\n", r.URL.Path)
 
-	w.Header().Set("Content-Type", "application/json")
+	//extract country name from the URL
+	countryName := strings.TrimSpace(r.URL.Path[len("/countryinfo/v1/info/"):])
+	if countryName == "" {
+		http.Error(w, "Missing country code", http.StatusBadRequest)
+		return
+	}
+
+	// fetch country info
+	country, err := fetchCountryInfo(countryName)
+	if err != nil {
+		http.Error(w, "Country not found:(", http.StatusNotFound)
+		return
+	}
 
 	//extract country code from the URL
 
@@ -22,16 +36,17 @@ func countryInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 		if !ok {
 			http.Error(w, "Coun	try not found", http.StatusNotFound)
-	 */
+
 			return
 		}
 
-	*/
+
 
 	if r.URL.Path == "/countryinfo/v1/info/" {
 		http.Error(w, "Missing country code", http.StatusBadRequest)
 		return
 	}
+	*/
 
 	/**	code := strings.TrimPrefix(r.URL.Path, "/countryinfo/v1/info/")
 	if code == "" {
@@ -45,10 +60,10 @@ func countryInfoHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Conent-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
 
 	*/
+	w.Header().Set("Conent-Type", "application/json")
+	json.NewEncoder(w).Encode(country)
 
 }
 
